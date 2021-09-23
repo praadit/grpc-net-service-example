cf2b89627ee0bf7b683da1d8dca9d4556aeb21bd
M. Aditya Tegar Pratama <tegar@agate.id>
2021-09-23 10:39:20 +0700

    add server stream example

--- GrpcClient/Program.cs
+++ GrpcClient/Program.cs
@@ -2,6 +2,7 @@
 using System.Net.Http;
 using System.Threading.Tasks;
 using Grpc.Net.Client;
+using Grpc.Core;
 
 namespace GrpcClient
 {
@@ -12,15 +13,22 @@ namespace GrpcClient
             using var channel = GrpcChannel.ForAddress("https://localhost:6001");
             var client = new Greeter.GreeterClient(channel);
             var reply = await client.SayHelloAsync(new HelloRequest{Name = "I'am Client"});
-
             Console.WriteLine("Greeting : "+ reply.Message);
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
 
             var mathClient = new Math.MathClient(channel);
-            var mathReply = await mathClient.AdditionAsync(new MathRequest{Number1 = 1, Number2 = 2});
-            
+            var mathReply = await mathClient.AdditionAsync(new MathRequest{Number1 = 1, Number2 = 2});            
             Console.WriteLine("1 + 2 = "+ mathReply.Summary);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+            
+            using var incrementCall = mathClient.Increment(new IncrementRequest{Start = 1, Limit = 8});
+            await foreach(var response in incrementCall.ResponseStream.ReadAllAsync()){
+                Console.WriteLine(response.Result);
+            }
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
         }
     }
 }
--- GrpcServer/Services/MathService.cs
+++ GrpcServer/Services/MathService.cs
@@ -11,13 +11,21 @@ namespace GrpcServer
         {
             _logger = logger;
         }
-        public override Task<MathReply> Addition(MathRequest request, ServerCallContext context){       
-            _logger.LogInformation("masuk");
-
+        public override Task<MathReply> Addition(MathRequest request, ServerCallContext context){
             return Task.FromResult(new MathReply
             {
                 Summary = request.Number1 + request.Number2
             });
+        }
+
+        public override async Task Increment(IncrementRequest request, IServerStreamWriter<IncrementResponse> responseStream,  ServerCallContext context){
+            for (int i = 0; i < request.Limit; i++)
+            {
+                await responseStream.WriteAsync(new IncrementResponse{
+                    Result = request.Start + i,
+                });
+                await Task.Delay(1000);
+            }
         }
     }
 }
